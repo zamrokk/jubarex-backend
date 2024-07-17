bf1d7f1a53fe4cd22d79bbf9bcee65fe480a1fb8
Benjamin Fuentes <zamrokk@gmail.com>
2024-07-17 10:58:24 +0200

    move to alicloud bucket

--- src/artefact/artefact.service.ts
+++ src/artefact/artefact.service.ts
@@ -30,16 +30,18 @@ export class ArtefactService {
   ) {
     // a client can be shared by different commands.
     this.client = new S3Client({
+      endpoint: process.env.S3_ENDPOINT,
       region: process.env.S3_REGION,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
       },
     } as S3ClientConfig);
   }
 
   async upload(file: Express.Multer.File): Promise<string> {
-    const id = uuid() + '.' + file.filename.split('.').pop();
+    console.log('file', file);
+    const id = uuid() + '.' + file.originalname.split('.').pop();
 
     console.log('uuid', id, 'file', file, 'mime', file.mimetype);
 
